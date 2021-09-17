33e6ad3d119f85a21bb7537dfe191c1c252d762f
Tanaka Seigo <seigo@1ft-seabass.jp>
2021-09-17 10:20:41 +0900

    Bootstrap の導入

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,5 +1,37 @@
 <script>
-	let name = 'world';
+    let name = 'Node-RED';  // world -> Node-RED
 </script>
-
-<h1>Hello {name}!</h1>
+ 
+<div class="container">
+  <div class="row">
+    <div class="row">
+      <div class="col-sm">
+        <h1>Hello {name}!</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
--- svelte-app/src/main.js
+++ svelte-app/src/main.js
@@ -1,7 +1,9 @@
 import App from './App.svelte';
-
+ 
 var app = new App({
-	target: document.body
+  // target: document.body
+  // Bootstrap の JavaScript 発動を body 下部で行うために修正
+  target: document.getElementById("app_svelte")
 });
-
+ 
 export default app;
